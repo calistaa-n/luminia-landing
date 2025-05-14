247af55bd8a204b12847f3346ecc6be32b42269d
calistaa-n <calistaayeo@gmail.com>
2025-05-14 18:35:09 +0800

    Fixed Typography on problems

--- src/components/problems.jsx
+++ src/components/problems.jsx
@@ -1,49 +1,7 @@
-// export default function Problems() {
-//   return (
-//     <section id="products" className="bg-white text-center p-20">
-//       <h2 className="text-5xl font-bold text-dGreen mb-15">Our Products</h2>
-
-//       <div className="flex flex-col lg:flex-row items-center justify-center gap-10">
-
-//         {/* Plastic Waste */}
-//         <div className="text-left max-w-md">
-//           <div className="relative w-full h-64">
-//             <img 
-//               src="/plastic.jpg" 
-//               alt="plastic bottles" 
-//               className="w-full h-64 object-cover rounded-xl" 
-//             />
-//           </div>
-//           <h3 className="text-4xl font-bold text-dGreen mt-4">Plastic Waste</h3>
-//           <p className="text-dGray">
-//             Our eco-conscious packaging reduces plastic waste.
-//           </p>
-//         </div>
-
-//         {/* Skin Problems */}
-//         <div className="text-left max-w-md">
-//           <div className="relative w-full h-64 space-x-10">
-//             <img 
-//               src="/acne.jpeg" 
-//               alt="a woman with acne" 
-//               className="w-full h-64 object-cover rounded-xl" 
-//             />
-//           </div>
-//           <h3 className="text-4xl font-bold text-dGreen mt-4">Skin Problems</h3>
-//           <p className="text-dGray">
-//             Combat acne, dryness, and dullness with our nutrient-rich drinks.
-//           </p>
-//         </div>
-
-//       </div>
-//     </section>
-//   );
-// }
-
 export default function Problems() {
   return (
     <section id="products" className="bg-white text-center p-20">
-      <h2 className="text-5xl font-bold text-dGreen mb-15">Our Products</h2>
+      <h2 className="text-5xl font-bold text-dGreen mb-15">What We're Solving</h2>
 
       <div className="flex flex-col lg:flex-row items-center justify-center gap-x-30 gap-y-15">
         
